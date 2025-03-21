505ea80bff7c18454c307f9134861129847fdfad
yumemiru-masomi <yumemi.saito@blueish.co.jp>
2025-03-21 16:25:25 +0900

    feat: updateTodoとdeleteTodoを追加

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/sqlite"
@@ -38,10 +39,60 @@ func createTodo(c *gin.Context) {
 	c.JSON(http.StatusCreated, todo)
 }
 
+func updateTodo(c *gin.Context){
+	idParam := c.Param("id") 
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+		return
+	}
+	
+	var todo Todo
+	if err := db.First(&todo, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Todo not found"})
+		return
+	}
+
+	//リクエストボディを一時的な変数にバインドする
+	var input Todo
+	if err := c.ShouldBindJSON(&input); err != nil {
+	  //ShouldBindJSONは、JSONを受け取ってTodo構造体に値を入れる（title, completed など）
+	  if err := c.ShouldBindJSON(&input); err != nil {
+		  c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		  return
+	  }
+    }
+
+	//値を更新
+    todo.Title = input.Title
+	todo.Completed = input.Completed
+
+	db.Save(&todo)
+	c.JSON(http.StatusOK, todo)
+}
+
 func getTodos(c *gin.Context) {
 	var todos []Todo
 	db.Find(&todos)
 	c.JSON(http.StatusOK, todos)
+}
+
+func deleteTodo(c *gin.Context){
+	idParam := c.Param("id") 
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+		return
+	}
+	
+	var todo Todo
+	if err := db.First(&todo, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Todo not found"})
+		return
+	}
+
+	db.Delete(&todo)
+	c.JSON(http.StatusOK, gin.H{"message": "Todo deleted"})
 }
 
 func main() {
@@ -51,8 +102,13 @@ func main() {
 	// 新規追加
 	r.POST("/todos", createTodo)
 
+	r.PUT("/todos/:id", updateTodo)
+
 	// 一覧取得
 	r.GET("/todos", getTodos)
+
+	// 削除
+	r.DELETE("/todos/:id", deleteTodo)
 
 	// サーバー起動
 	r.Run(":8090")
